6424d0f9223f7e60bfff014bc6dc3054a8799427
Roman Yurchenko <yurch.html@gmail.com>
2016-08-17 18:22:34 +0300

    wash

---
+++ portfolio/wash/prix.php
@@ -0,0 +1,171 @@
+<?php include 'header.php';?>
+
+<div class="b111">
+
+
+<div class="asss">
+
+<span class="bq">Цены:</span>
+<span class="bq1">Ремонт стиральных машин Киев / Цены</span>
+
+</div>
+
+
+
+
+</div>
+
+ <div class="main">
+
+ 
+ 
+ 
+<div class= "main2">
+<div class="bs">
+
+ <div class="body">
+ 
+  
+
+<div class="cena">
+<h3>Предворительные цены на ремонт</h3>
+<p>
+Стоимость ремонта стиральной машины складывается из разных факторов. Сложность и объем работ, стоимость запчастей – все это существенно влияет на конечную сумму. По этой причине я не работаю с фиксированным прайсов на услуги, как это делают некоторые мои коллеги. Что не совсем справедливо, в первую очередь, по отношению к клиенту. Одна и та же операция, на машинках с разными конструктивными особенностями может занять 5 минут, а может полчаса. С фиксированной ценой ремонта стиральной машины за несложные манипуляции, которые займут несколько минут, с вас возьмут как за получасовую работу.
+</p><br>
+<table cellpadding="2">
+  <tr class="td">
+    <td >Диагностика(без ремонта)</td>
+    <td>60 грн</td>
+   
+  </tr>
+ <tr>
+    <td >Замена амортизатора </td>
+    <td>200 грн</td>
+  </tr>
+   <tr class="td">
+    <td>Замена замка </td>
+    <td>190 грн</td>
+  </tr>
+   <tr>
+    <td>Замена клапана </td>
+    <td>160 грн</td>
+  </tr>
+  
+  <tr class="td">
+    <td>Замена крепления люка, стекла, ручки </td>
+    <td>160грн</td>
+  </tr>
+  
+  
+  <tr>
+    <td>Замена манжета люка </td>
+    <td>150 грн</td>
+  </tr>
+  
+  <tr class="td">
+    <td>Замена подшипника  </td>
+    <td>450 грн</td>
+  </tr>
+  
+  <tr>
+    <td>Замена ремня </td>
+    <td>180 грн</td>
+  </tr>
+  
+  
+  <tr class="td">
+    <td>Замена сливного насоса </td>
+    <td>150 грн</td>
+  </tr>
+  
+  <tr>
+    <td>Замена тэна </td>
+    <td>150 грн</td>
+  </tr>
+  
+  
+  <tr class="td">
+    <td>Замена эл. модуля, блока индикации  </td>
+    <td>150 грн</td>
+  </tr>
+  
+  
+  <tr >
+    <td>Ремонт электронного модуля управления </td>
+    <td>150 грн</td>
+  </tr>
+  
+  
+  <tr class="td">
+    <td>Чистка фильтра заливного шланга или фильтра сливного насоса  </td>
+    <td>150 грн</td>
+  </tr>
+  
+   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+</table>
+<br>
+<p class="negr1">
+
+*При отказе от ремонта стирально машины оплачивается выезд специалиста и диагностика в размере 100 грн.<br>
+*Цены указаны без учета запчастей.<br>
+</p>
+
+
+
+
+
+
+
+
+
+</div>
+ 
+ 
+ 
+
+</div>
+<div class="sidebar">
+<?php include 'sidebar.php';?>
+ 
+ <div>
+<!-- VK Widget -->
+<div id="vk_groups"></div>
+<script type="text/javascript">
+VK.Widgets.Group("vk_groups", {mode: 0, width: "215", height: "400", color1: '#F4F4F4', color2: '2B587A', color3: '5B7FA6'}, 104061688);
+</script>
+</div >
+ 
+ </div>
+</div>
+ 
+  </div>
+ </div>
+  <div style="clear:both"></div>
+  <script type="text/javascript">(function() {
+  if (window.pluso)if (typeof window.pluso.start == "function") return;
+  if (window.ifpluso==undefined) { window.ifpluso = 1;
+    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+    var h=d[g]('body')[0];
+    h.appendChild(s);
+  }})();</script>
+  <?php include 'footer.php';?>
+ 
+
+ 
+
+ </body>
+
+</html>
---
+++ portfolio/wash/sidebar.php
@@ -0,0 +1,37 @@
+<?php
+echo '<p class="s3">Самостоятельный ремонт</p><br>
+<iframe width="220" height="200" src="https://www.youtube.com/embed/Cc3YfykFKXM" frameborder="0" allowfullscreen></iframe>
+
+  <div><br>
+ <p class="s3">Полезные стьтьи </p>
+ <ul>
+<li><a href="">Типы стиральных машин</a></li>
+<li><a href="">Распространенные неисправности </a></li>
+<li><a href="">При какой темперетуре стирать</a> </li>
+<li><a href="">Коды ошибок </a></li>
+<li><a href="">Рекомендации по эксплуатации </a></li>
+<li><a href="">Мифы о стирке</a></li>
+</ul>
+ </div>
+ <div class="s3">
+<p class="s3"> Наши преимущества </p>
+ <ul><li>Быстрое и качественное  <br>
+ устранение неполадок</li>
+<li>Демократичные цены. </li>
+<li>Качество. </li>
+<li>Гарантия на выполненную работу. </li>
+<li>Выезд и диагностика Бесплатно.
+<li>Опытные мастера.</li></ul>
+</div> 
+
+';
+
+
+
+?>
+<?php include 'forma.html';?>
+
+
+
+
+
